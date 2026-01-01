db404c4dbc46b541aa0186d3f73bb52d5229f3cd
PUNEETH KUMAR <puneethkumarg96@gmail.com>
2026-01-01 12:11:54 +0530

    feat: added the new PostApi

--- src/main/java/com/example/demo/apis/HelloController.java
+++ src/main/java/com/example/demo/apis/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo.apis;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World";
+    }
+
+    @PostMapping("/greet")
+    public String greet() {
+        return "Greetings from Spring Boot!";
     }
 }
