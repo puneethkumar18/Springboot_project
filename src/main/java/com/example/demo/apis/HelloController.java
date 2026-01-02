0f090031cb18e28fd4098607ee023618ec446214
PUNEETH KUMAR <puneethkumarg96@gmail.com>
2026-01-02 18:09:44 +0530

    How to render HTML Page

--- src/main/java/com/example/demo/apis/HelloController.java
+++ src/main/java/com/example/demo/apis/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.demo.apis;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +11,7 @@ public class HelloController {
         return "Hello, World";
     }
 
-    @PostMapping("/greet")
+    @GetMapping("/greet")
     public String greet() {
         return "Greetings from Spring Boot!";
     }
